cf142527811fc02ce0fa5a54b1fa94707ee351d3
midnightchip <34963874+midnightchip@users.noreply.github.com>
2018-02-23 08:05:01 -0700

    Update uninstall.sh

--- Ext3nder-Installer-Electrafied/uninstall.sh
+++ Ext3nder-Installer-Electrafied/uninstall.sh
@@ -2,6 +2,8 @@ echo "Removing the files"
 rm -rf /Applications/Ext3nder.app
 rm  /Library/SBInject/Ext3nder_SpringBoard.dylib
 rm  /Library/SBInject/Ext3nder_SpringBoard.plist
+rm /bootstrap/bin/zip
+rm /bootstrap/bin/unzip
 echo 'Running postrm!'
 chmod +x Ext3nder-Installer-Electrafied/postrm
 /Ext3nder-Installer-Electrafied/postrm
